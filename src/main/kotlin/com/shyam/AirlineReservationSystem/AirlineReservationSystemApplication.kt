c0fcb94e2aa8c02f0a37a981689f9d3e1e863b05
shyam <shyamsam1818@gmail.com>
2024-10-27 16:32:08 +0530

    first

---
+++ src/main/kotlin/com/shyam/AirlineReservationSystem/AirlineReservationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.shyam.AirlineReservationSystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirlineReservationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirlineReservationSystemApplication>(*args)
+}
